47485e24dac6f06da6d45557fa92292dded4ba42
Cole Webster <CWebster_Coding@outlook.com>
2022-08-18 10:15:07 -0400

    Updating with the styling and template for the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,12 @@ import "./style.css";
 
 export default function Header() {
   return (
-    <div className="header">
-      <img src={Logo} className="headerLogo" />
-      <button>Log In</button>
-      <button>Join!</button>
-      
-    </div>
-  );
+   <header className="Header">
+      <img src={Logo} className="Logo" alt="logo" />
+      <nav className="Nav">
+        <button className="LogIn">Log In</button>
+        <button className="Join">Join Now! </button>
+      </nav>
+   </header>
+  )
 }
